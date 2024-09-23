c4ecbbf555b5a478fee04c03c3b25c6b0466426c
Timur Abdullin <timur.abdullin.2406@mail.ru>
2024-09-23 22:36:41 +0300

    writing the base code in main and config

---
+++ main.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from config_data.config import Config, load_config
+import asyncio
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    logging.basicConfig(level=logging.DEBUG, format='{filename}:{lineno} #{levelname:8} '
+                        '[{asctime}] - {name} - {message}', style='{')
+
+    logger.info('Starting my bot')
+    config: Config = load_config()
+    bot = Bot(token=config.tg_bot.token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+asyncio.run(main())
